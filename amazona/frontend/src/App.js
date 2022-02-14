9ae6a25d32bdd37d9a67bc3f088dc2e927c8fc30
azmir849 <azmir15-849@diu.edu.bd>
2022-02-14 09:24:21 +0600

    amazona ecommerce mern app created

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>amazona</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
