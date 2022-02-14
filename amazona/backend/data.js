d2caee5ce3fd5bd55b0c3dbe9a74cf424edef71f
azmir849 <azmir15-849@diu.edu.bd>
2022-02-15 01:37:39 +0600

    add to cart functionality

--- amazona/backend/data.js
+++ amazona/backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: "1",
       name: "Nike Slim Shirt",
       slug: "nike-slim-shirt",
       category: "Shirts",
@@ -13,6 +14,7 @@ const data = {
       description: "high quality shirt",
     },
     {
+      _id: "2",
       name: "Adidas Fit Shirt",
       slug: "adidas-fit-shirt",
       category: "Shirts",
@@ -25,6 +27,7 @@ const data = {
       description: "high quality product",
     },
     {
+      _id: "3",
       name: "Nike Slim Pant",
       slug: "nike-slim-pant",
       category: "Pants",
@@ -37,7 +40,8 @@ const data = {
       description: "high quality shirt",
     },
     {
-      name: "Adida Fit Pant",
+      _id: "4",
+      name: "Adidas Fit Pant",
       slug: "adidas-fit-pant",
       category: "Pants",
       image: "/images/p4.jpg",
