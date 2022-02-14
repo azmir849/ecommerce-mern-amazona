c777cf457007ee1b451447e1ed67a6c5f88c4e9f
azmir849 <azmir15-849@diu.edu.bd>
2022-02-14 10:16:51 +0600

    list of product show in home page

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', //679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 200,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality proiduct'
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 240,
+            countInStock: 4,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'high quality shirt'
+        },
+    ],
+}
+
+export default data;
